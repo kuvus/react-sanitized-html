04b6bbbfd3ecce61e1537c903cbfb31f7aa54ef6
kuvuś <kuba17ism@tuta.io>
2021-01-03 00:05:32 +0100

    Add option to limit number of output characters

--- __tests__/render.js
+++ __tests__/render.js
@@ -1,26 +1,33 @@
-'use strict';
+'use strict'
 
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import SanitizedHTML from '../';
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import SanitizedHTML from '../'
 
 describe('SanitizedHTML', () => {
-  test('should render tag correctly', () => {
-    expect(
-      ReactDOMServer.renderToStaticMarkup(
-        <SanitizedHTML html={ '<a href="http://bing.com/">Bing</a>' }/>
-      )
-    ).toBe('<div><a href="http://bing.com/">Bing</a></div>');
-  });
+    test('should render tag correctly', () => {
+        expect(
+            ReactDOMServer.renderToStaticMarkup(<SanitizedHTML html={'<a href="http://bing.com/">Bing</a>'} />)
+        ).toBe('<div><a href="http://bing.com/">Bing</a></div>')
+    })
 
-  test('should render only allowed tags', () => {
-    expect(
-      ReactDOMServer.renderToStaticMarkup(
-        <SanitizedHTML
-          allowedTags={['a']}
-          html={ '<a href="http://bing.com/"><strong>Bing</strong></a>' }
-        />
-      )
-    ).toBe('<div><a href="http://bing.com/">Bing</a></div>');
-  });
-});
+    test('should render only allowed tags', () => {
+        expect(
+            ReactDOMServer.renderToStaticMarkup(
+                <SanitizedHTML allowedTags={['a']} html={'<a href="http://bing.com/"><strong>Bing</strong></a>'} />
+            )
+        ).toBe('<div><a href="http://bing.com/">Bing</a></div>')
+    })
+
+    test('should render only provided number of characters', () => {
+        expect(
+            ReactDOMServer.renderToStaticMarkup(
+                <SanitizedHTML
+                    allowedTags={['a']}
+                    html={'<a href="http://bing.com/"><strong>Bing</strong></a>'}
+                    finalLength={10}
+                />
+            )
+        ).toBe('<div><a href="h</div>')
+    })
+})
--- src/index.js
+++ src/index.js
@@ -1,69 +1,70 @@
 import PropTypes from 'prop-types'
-import React from 'react';
-import sanitizeHTML from 'sanitize-html';
+import React from 'react'
+import sanitizeHTML from 'sanitize-html'
 
 const SUPPORTED_SANITIZER_OPTIONS = [
-  'allowProtocolRelative',
-  'allowedAttributes',
-  'allowedClasses',
-  'allowedSchemes',
-  'allowedSchemesByTag',
-  'allowedTags',
-  'exclusiveFilter',
-  'nonTextTags',
-  'parser',
-  'selfClosing',
-  'transformTags'
-];
+    'allowProtocolRelative',
+    'allowedAttributes',
+    'allowedClasses',
+    'allowedSchemes',
+    'allowedSchemesByTag',
+    'allowedTags',
+    'exclusiveFilter',
+    'nonTextTags',
+    'parser',
+    'selfClosing',
+    'transformTags',
+]
 
 const SanitizedHTML = props => {
-  const sanitizerOptions = SUPPORTED_SANITIZER_OPTIONS.reduce((options, name) => {
-    const value = props[name];
+    const sanitizerOptions = SUPPORTED_SANITIZER_OPTIONS.reduce((options, name) => {
+        const value = props[name]
 
-    if (typeof value !== 'undefined') {
-      options[name] = value;
-    }
+        if (typeof value !== 'undefined') {
+            options[name] = value
+        }
 
-    return options;
-  }, {});
+        return options
+    }, {})
 
-  // TODO: Add memoization
-  const sanitizedHTML = sanitizeHTML(
-    props.html,
-    sanitizerOptions
-  );
+    // TODO: Add memoization
+    let sanitizedHTML = props.finalLength
+        ? sanitizeHTML(props.html, sanitizerOptions).slice(0, props.finalLength)
+        : sanitizeHTML(props.html, sanitizerOptions)
 
-  return (
-    <div
-      className={ props.className }
-      dangerouslySetInnerHTML={{ __html: sanitizedHTML }}
-      id={ props.id }
-      style={ props.style }
-    />
-  );
+    return (
+        <div
+            className={props.className}
+            dangerouslySetInnerHTML={{ __html: sanitizedHTML }}
+            id={props.id}
+            style={props.style}
+        />
+    )
 }
 
 SanitizedHTML.defaultProps = {
-  html: ''
-};
+    html: '',
+}
 
 SanitizedHTML.propTypes = {
-  allowProtocolRelative: PropTypes.bool,
-  allowedAttributes    : PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
-  allowedClasses       : PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
-  allowedSchemes       : PropTypes.arrayOf(PropTypes.string),
-  allowedSchemesByTag  : PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
-  allowedTags          : PropTypes.arrayOf(PropTypes.string),
-  exclusiveFilter      : PropTypes.func,
-  html                 : PropTypes.string.isRequired,
-  nonTextTags          : PropTypes.arrayOf(PropTypes.string),
-  parser               : PropTypes.any,
-  selfClosing          : PropTypes.arrayOf(PropTypes.string),
-  transformTags        : PropTypes.objectOf(PropTypes.oneOf([PropTypes.func, PropTypes.string])),
+    allowProtocolRelative: PropTypes.bool,
+    allowedAttributes: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+    allowedClasses: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+    allowedSchemes: PropTypes.arrayOf(PropTypes.string),
+    allowedSchemesByTag: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+    allowedTags: PropTypes.arrayOf(PropTypes.string),
+    exclusiveFilter: PropTypes.func,
+    html: PropTypes.string.isRequired,
+    nonTextTags: PropTypes.arrayOf(PropTypes.string),
+    parser: PropTypes.any,
+    selfClosing: PropTypes.arrayOf(PropTypes.string),
+    transformTags: PropTypes.objectOf(PropTypes.oneOf([PropTypes.func, PropTypes.string])),
 
-  className: PropTypes.string,
-  id       : PropTypes.string,
-  style    : PropTypes.any
-};
+    className: PropTypes.string,
+    id: PropTypes.string,
+    style: PropTypes.any,
+
+    finalLength: PropTypes.number,
+}
 
 export default SanitizedHTML
